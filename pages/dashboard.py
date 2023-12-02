58be45a4d646449df3911fe63dc87a48664c0e9d
kurama8103 <38579582+kurama8103@users.noreply.github.com>
2023-12-03 08:43:26 +0900

    f

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,7 +1,8 @@
+import japanize_matplotlib
 from pypfopt.efficient_frontier import EfficientFrontier
 from pypfopt import risk_models, expected_returns, plotting
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression, Ridge, Lasso,LassoCV
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, LassoCV
 
 import pandas as pd
 import streamlit as st
@@ -9,9 +10,7 @@ import streamlit as st
 import seaborn as sns
 sns.set_style('whitegrid')
 
-import japanize_matplotlib
 japanize_matplotlib.japanize()
-
 
 
 def load_csv():
@@ -100,12 +99,12 @@ def render_pf_opt():
     df_ = load_csv()
     if df_ is not None:
         st.text('graph')
-        flg=show_graphs(df_)
+        flg = show_graphs(df_)
 
         st.text('Return of assets')
         show_returns(df_)
 
-        calc_regression(df_,flg)
+        # calc_regression(df_, flg)
 
         st.text('mean and volatility')
         df_assets = pd.DataFrame({
@@ -126,64 +125,70 @@ def render_pf_opt():
         for k, v in res_opt.items():
             st.write(k)
             st.bar_chart(pd.DataFrame(v['weight'], df_.columns), width=50,)
-        
+
         st.json(res_opt)
+
 
 def show_returns(df_):
     fig, ax = plt.subplots()
     _ = df_.iloc[-1]/df_.iloc[[-2, -4, -7, -13]]-1
-    _.index = ['1M', '3M', '6M','12M']
+    _.index = ['1M', '3M', '6M', '12M']
     sns.heatmap(_.T, center=0,
                 annot=True, fmt='.1%', cmap='PiYG')
     st.write(fig)
 
+
 def show_graphs(df_):
     if st.checkbox('price 1'):
-        df_=df_/df_.iloc[0]
-    
-    flg=0
-    if st.checkbox('pct_change'):
-        df_=df_.pct_change().dropna()
-        flg=1
+        df_ = df_/df_.iloc[0]
 
-    st.line_chart(df_,height=200)
+    flg = 0
+    if st.checkbox('pct_change'):
+        df_ = df_.pct_change().dropna()
+        flg = 1
+
+    st.line_chart(df_, height=200)
     st.write(format_df(df_))
     return flg
 
-def calc_regression(df_,flg=0):
-    y=st.selectbox('Y',df_.columns)
-    c=list(df_.columns.drop(y))
-    x=st.multiselect('X',c,c)
-    if len(x)==0:
+
+def calc_regression(df_, flg=0):
+    y = st.selectbox('Y', df_.columns)
+    c = list(df_.columns.drop(y))
+    x = st.multiselect('X', c, c)
+    if len(x) == 0:
         st.stop()
 
     model = LinearRegression(fit_intercept=True, normalize=False)
-    res=summary_model_sk(model,df_[x],df_[y])
-    st.write(model,res['score'])
-    st.json(res,expanded=False)
+    res = summary_model_sk(model, df_[x], df_[y])
+    st.write(model, res['score'])
+    st.json(res, expanded=False)
 
-    model = LassoCV(fit_intercept=True, normalize=False,alphas=[0,0.01,0.1,1,10],cv=5)
-    res=summary_model_sk(model,df_[x],df_[y])
-    st.write(model,res['score'])
-    st.json(res,expanded=False)
+    model = LassoCV(fit_intercept=True, normalize=False,
+                    alphas=[0, 0.01, 0.1, 1, 10], cv=5)
+    res = summary_model_sk(model, df_[x], df_[y])
+    st.write(model, res['score'])
+    st.json(res, expanded=False)
 
-    pred=(df_[x]*res['coef']).sum(axis=1)+res['intercept']
-    pred.name='prediction'
-    pred=pd.concat([pred,df_[y]],axis=1)
-    if flg==1:
-        pred=(1+pred).cumprod()
-    st.line_chart(pred,height=200)
+    pred = (df_[x]*res['coef']).sum(axis=1)+res['intercept']
+    pred.name = 'prediction'
+    pred = pd.concat([pred, df_[y]], axis=1)
+    if flg == 1:
+        pred = (1+pred).cumprod()
+    st.line_chart(pred, height=200)
 
-def summary_model_sk(model,x,y):
+
+def summary_model_sk(model, x, y):
     model.fit(x, y)
     res = {
-#        'model': model,
+        #        'model': model,
         'score': model.score(x, y),
         'intercept': model.intercept_,
         'coef': model.coef_.tolist(),
-#        'predict':model.predict(x),
+        #        'predict':model.predict(x),
         'params': model.get_params()
     }
     return res
+
 
 render_pf_opt()
