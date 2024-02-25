9b5f8167649920612353320174d240e2a3f81f00
kurama8103 <38579582+kurama8103@users.noreply.github.com>
2024-02-25 23:30:37 +0900

    revise

--- pages/fin_single.py
+++ pages/fin_single.py
@@ -5,8 +5,36 @@ import quantstats as qs
 import pandas as pd
 import numpy as np
 import streamlit as st
-import matplotlib.pyplot as plt
+import io
+import os
+import pandas_datareader.data as web
+from numpy.lib.stride_tricks import sliding_window_view
 from st_util import load_csv
+
+# import myfunc.finfunc as mff
+
+
+@st.cache(allow_output_mutation=True, show_spinner=False)
+def get_indices(flg: bool = False):
+    fn = "dic_fred.pickle"
+    end = pd.Timestamp.today() + pd.Timedelta(days=0)
+    start = pd.to_datetime("2020-01-01")
+    dic_ticker = {
+        "S&P500": "SP500",
+        "NASDAQ_Composit": "NASDAQCOM",
+    }
+    if os.path.exists(fn) and not (flg):
+        print("load")
+        df = pd.read_pickle(fn)
+
+    else:
+        print("download")
+
+    df = web.DataReader(dic_ticker.values(), "fred", start, end)
+    df.columns = dic_ticker.keys()
+
+    # df.to_pickel(fn)
+    return df
 
 
 def format_df(df, digit=4):
@@ -15,24 +43,108 @@ def format_df(df, digit=4):
     return df
 
 
-def tsa_render(df_):
-    qs.extend_pandas()
-    n_pred = 10
-    if df_ is not None:
-        df = df_[st.selectbox("select data", df_.columns)]
-        # bm = st.selectbox(
-        #     "select benchmark data", [None] + list(df_.columns.drop(df.name))
-        # )
-        # if bm is not None:
-        #     df_bm = df_[bm]
-        # else:
-        #     df_bm = None
+# def mape(pred, true):
+#     return np.mean(np.abs((pred - true) / true)) * 100
 
+
+def momentum(x, window=None):
+    if window == None:
+        window = len(x)
+    r = np.cumprod(x[-window - 2 : -1] + 1)
+    mom = r[-1] / r[0] - 1
+    return x[-1] - mom
+
+
+def moving_window(x, window: int, func) -> list:
+    return [func(d) for d in sliding_window_view(x, window)]
+
+
+def moving_window_df(x: pd.DataFrame, window: int, func) -> pd.DataFrame:
+    try:
+        return pd.DataFrame(
+            moving_window(x.values, window, func),
+            index=x.tail(len(x) + 1 - window).index,
+        )
+    except:
+        return None
+
+
+@st.cache(show_spinner=False)
+def qs_html(df: pd.DataFrame, download_filename, benchmark=None):
+    qs.reports.html(
+        df,
+        # rf=0.0,
+        title=df.name,
+        download_filename=download_filename,
+        output="./tmp/",
+        benchmark=benchmark,
+    )
+
+    with open(download_filename) as f:
+        s = f.read()
+
+    return s
+
+
+# @st.cache(show_spinner=True, allow_output_mutation=True)
+# def predict_darts(series, n_pred=10, w=20):
+#     from darts.models import NaiveDrift, NaiveSeasonal, ExponentialSmoothing
+#     from darts.models import RandomForest, RegressionModel, AutoARIMA, Prophet
+#     from darts import TimeSeries
+
+#     train = TimeSeries.from_series(series)[-w:]
+
+#     models = [
+#         # NaiveDrift(n_pred),
+#         NaiveSeasonal(n_pred),
+#         AutoARIMA(),
+#         # ExponentialSmoothing(),
+#         # Prophet(),
+#         # RandomForest(lags=5),
+#         RegressionModel(lags=5),
+#     ]
+
+#     pred = pd.DataFrame()
+#     for model in models:
+#         p_ = model.fit(series=train).predict(n_pred).pd_series()
+#         s = str(model)
+#         if s.find("(") > 0:
+#             s = s[: s.find("(")]
+#         p_.name = s
+#         pred = pd.concat([pred, p_], axis=1)
+#     pred.index = pd.to_datetime(pred.index)
+#     return pred
+
+
+def _f(x):
+    n = 252
+    m = np.mean(x) * n
+    s = np.std(x) * np.sqrt(n)
+    d = {
+        "rolling_return": m,
+        "standard deviation": s,
+        "sharpe ratio": (m / s),
+        # "skew": scipy.stats.skew(x),
+        # "kurt": scipy.stats.kurtosis(x),
+        # "hurst": mff.hurst(x)[0],
+        "momentum": momentum(x),
+        # "cvar": mff.cvar(x)[1],
+        # "adf":mff.adf_summary(x)['adf'],
+        # "max_draw_down": np.cumprod(x + 1)[-1] / np.cumprod(x + 1).max() - 1,
+    }
+    return d
+
+
+def tsa_render(df):
+    h = 150
+    window = 180
+    qs.extend_pandas()
+    if df is not None:
         st.markdown("### Input data")
-        df_ = df
+        code = st.selectbox("select data", df.columns)
+        st.line_chart(df[code], height=h)
         d1 = df.index[0].to_pydatetime()
         d2 = df.index[-1].to_pydatetime()
-
         d1, d2 = st.slider(
             "period",
             value=(d1, d2),
@@ -41,14 +153,15 @@ def tsa_render(df_):
             format="Y/M/D",
             step=pd.Timedelta(days=1),
         )
-        df_ = df.loc[d1:d2]
+        df_ = df[code].loc[d1:d2]
 
-        if min(df_) < 0:
+        if df_.min() < 0:
             pass
         else:
-            dfr = df_.pct_change().dropna()
-            dfr.name = "cum_ret"
+            dfr = df_.pct_change().fillna(0)
+            dfr.name = "cumulative_return"
 
+        # st.dataframe(dfr)
         st.text(
             # "From "
             # + df_.index[0].strftime("%Y-%m-%d")
@@ -62,123 +175,93 @@ def tsa_render(df_):
             + " years)"
         )
 
-        # st.line_chart(df, height=h)
-        # st.line_chart(dfr.rolling(30).std() * np.sqrt(250), height=h)
-        # st.line_chart(dfr.rolling(30).mean() / dfr.rolling(30).std(), height=h)
-
         # fig = plt.figure(figsize=(8, 2))
         # dfr.hist(bins=20)
         # st.pyplot(fig)
 
-        # import plotly.express as px
-        # st.plotly_chart(
-        #     px.histogram(y=dfr, histnorm="probability density"),
-        #     use_container_width=True,
-        #     sharing="streamlit",
-        #     theme="streamlit",
+        # met = qs.reports.metrics(dfr, display=False)
+        # st.dataframe(
+        #     (
+        #         met.loc[
+        #             [
+        #                 # "Start Period",
+        #                 # "End Period",
+        #                 # "Risk-Free Rate",
+        #                 "Cumulative Return",
+        #                 "All-time (ann.)",
+        #                 "CAGR﹪",
+        #                 "Sharpe",
+        #                 # "Prob. Sharpe Ratio",
+        #                 "MTD",
+        #                 "3M",
+        #                 "6M",
+        #                 "YTD",
+        #                 "1Y",
+        #                 "Max Drawdown",
+        #                 # "Longest DD Days",
+        #                 "Avg. Drawdown",
+        #                 # "Avg. Drawdown Days",
+        #             ]
+        #         ]
+        #     )
+        # )
+        # st.dataframe(
+        #     met.loc[
+        #         [
+        #             "Longest DD Days",
+        #             "Avg. Drawdown Days",
+        #         ]
+        #     ]
         # )
 
-        # l = [
-        #     "cagr",
-        #     "volatility",
-        #     "sharpe",
-        #     "max_drawdown",
-        #     "value_at_risk",
-        #     "expected_shortfall",
-        # ]
-        # d = {"Cumulative Return": (df[-1] / df[0] - 1)}
-        # for s in l:
-        #     t = eval("df.{}()".format(s))
-        #     d.update({s: t})
-        # st.dataframe(pd.Series(d, name="Value"))
+        # _ = pd.concat(
+        #     [
+        #         (dfr + 1).cumprod() - 1,
+        #         # mff.max_draw_down(df),
+        #         moving_window_df(dfr, window, _f),
+        #     ],
+        #     axis=1,
+        # )
 
-        # import myfunc.finfunc as mff
-        from numpy.lib.stride_tricks import sliding_window_view
-        import scipy
+        d = (dfr + 1).cumprod()
+        st.write(d.name.replace("_", " "))
+        d.name = code
+        st.line_chart(d - 1, height=h)
 
-        def momentum(x, window=None):
-            if window == None:
-                window = len(x)
-            r = np.cumprod(x[-window - 2 : -1] + 1)
-            mom = r[-1] / r[0] - 1
-            return x[-1] - mom
+        # d = mff.max_draw_down(df_)
+        d = d / d.rolling(len(d), min_periods=1).max() - 1
+        st.write("max draw down")
+        d.name = code
+        st.area_chart(d, height=h)
 
-        def moving_window(x, window: int, func) -> list:
-            return [func(d) for d in sliding_window_view(x, window)]
+        _ = df_.asfreq("D").ffill().asfreq("M").pct_change().dropna()
+        if len(_) > 0:
+            _.name = code
+            st.write("monthly return")
+            st.bar_chart(_, height=h)
 
-        def moving_window_df(x: pd.DataFrame, window: int, func) -> pd.DataFrame:
-            return pd.DataFrame(
-                moving_window(x.values, window, func),
-                index=x.tail(len(x) + 1 - window).index,
-            )
-
-        def _f(x):
-            n = 252
-            m = np.mean(x) * n
-            s = np.std(x) * np.sqrt(n)
-            d = {
-                "roll_ret": m,
-                "std": s,
-                "sharpe": (m / s),
-                "skew": scipy.stats.skew(x),
-                "kurt": scipy.stats.kurtosis(x),
-                # "hurst": mff.hurst(x)[0],
-                "mom": momentum(x),
-                # "cvar": mff.cvar(x)[1],
-                # "adf":mff.adf_summary(x)['adf'],
-            }
-            return d
-
-        met = qs.reports.metrics(dfr, display=False)
-        st.dataframe(
-            met.loc[
-                [
-                    # "Start Period",
-                    # "End Period",
-                    "Risk-Free Rate",
-                    "Cumulative Return",
-                    "All-time (ann.)",
-                    "CAGR﹪",
-                    "Sharpe",
-                    # "Prob. Sharpe Ratio",
-                    "MTD",
-                    "3M",
-                    "6M",
-                    "YTD",
-                    "1Y",
-                    "Max Drawdown",
-                    "Longest DD Days",
-                    "Avg. Drawdown",
-                    "Avg. Drawdown Days",
-                ]
-            ].round(2)
+        st.write("histgram(daily return%)")
+        count, division = np.histogram(dfr, bins=20)
+        st.bar_chart(
+            pd.Series(count, np.round(division[1:] * 100, 1), name=code), height=h
         )
 
-        _ = pd.concat(
-            [
-                (dfr + 1).cumprod(),
-                # mff.max_draw_down(df),
-                moving_window_df(dfr, 180, _f),
-            ],
-            axis=1,
-        )
-        h = 120
-        for c, d in _.items():
-            st.write(c)
-            st.line_chart(d, height=h)
+        _ = moving_window_df(dfr, window, _f)
+        if _ is not None:
+            for c, d in _.items():
+                st.write(c.replace("_", " ") + " ({}days)".format(window))
+                d.name = code
+                st.line_chart(d, height=h)
 
         # st.pyplot(qs.plots.monthly_heatmap(dfr, square=True, show=False))
 
-        count, division = np.histogram(dfr, bins=20)
-        st.bar_chart(
-            pd.Series(count, np.round(division[1:] * 100, 1), name="histgram"), height=h
-        )
+        # st.write("histgram(monthly return)")
+        # count, division = np.histogram(_, bins=10)
+        # st.bar_chart(
+        #     pd.Series(count, np.round(division[1:] * 100, 1), name=""), height=h
+        # )
 
-        _ = df.asfreq("D").ffill().asfreq("M").pct_change().dropna()
-        _.name = "month_ret"
-        st.bar_chart(_, height=h)
-
-        st.dataframe(format_df(_.dropna()), height=200)
+        # st.dataframe(format_df(_.dropna()), height=200)
 
         # st.markdown("### Predict")
         # pred = predict_darts(dfr.asfreq("B", method="ffill"), n_pred=n_pred)
@@ -208,11 +291,20 @@ def tsa_render(df_):
 
         # full report
         st.markdown("### Quantstats")
+        f = df_.name
+        bm = st.selectbox("select benchmark data", [None] + list(df.columns.drop(code)))
+        if bm is not None:
+            df_bm = df[bm]
+            f += "_" + bm
+            # df_ = pd.concat([df_, df_bm.loc[d1:d2]], axis=1)
+        else:
+            df_bm = None
+
         with st.spinner("Creating Full Analysis..."):
-            filename_html = "quantstats_" + df.name + ".html"
+            filename_html = "quantstats_{}.html".format(f)
             st.download_button(
                 "Download Quantstats Full Analysis",
-                data=qs_html(df_, filename_html, benchmark=None),
+                data=qs_html(df_, filename_html, benchmark=df_bm),
                 file_name=filename_html,
             )
 
@@ -222,62 +314,28 @@ def tsa_render(df_):
         )
 
 
-@st.cache(show_spinner=False)
-def qs_html(df: pd.DataFrame, download_filename, benchmark=None):
-    qs.reports.html(
-        df,
-        rf=0.0,
-        title=df.name,
-        download_filename=download_filename,
-        output="./tmp/",
-        benchmark=benchmark,
+df = get_indices()
+# df = load_csv()
+if df is None:
+    sample = """date	A	B
+    2020-01-01	100	100
+    2020-01-02	105	95
+    2020-01-03	108	105
+    2020-01-04	105	110
+    2020-01-05	110	105
+    2020-01-06	120	100
+    2020-01-07	110	95
+    2020-01-08	100	90
+    """
+    t = st.text_area("like csv", sample, placeholder=sample)
+    df = pd.read_csv(
+        io.StringIO(t.strip()), sep=None, index_col=0, parse_dates=True, engine="python"
     )
+    st.write(format_df(df))
 
-    with open(download_filename) as f:
-        s = f.read()
-
-    return s
-
-
-@st.cache(show_spinner=True, allow_output_mutation=True)
-def predict_darts(series, n_pred=10, w=20):
-    from darts.models import NaiveDrift, NaiveSeasonal, ExponentialSmoothing
-    from darts.models import RandomForest, RegressionModel, AutoARIMA, Prophet
-    from darts import TimeSeries
-
-    train = TimeSeries.from_series(series)[-w:]
-
-    models = [
-        # NaiveDrift(n_pred),
-        NaiveSeasonal(n_pred),
-        AutoARIMA(),
-        # ExponentialSmoothing(),
-        # Prophet(),
-        # RandomForest(lags=5),
-        RegressionModel(lags=5),
-    ]
-
-    pred = pd.DataFrame()
-    for model in models:
-        p_ = model.fit(series=train).predict(n_pred).pd_series()
-        s = str(model)
-        if s.find("(") > 0:
-            s = s[: s.find("(")]
-        p_.name = s
-        pred = pd.concat([pred, p_], axis=1)
-    pred.index = pd.to_datetime(pred.index)
-    return pred
-
-
-def mape(pred, true):
-    return np.mean(np.abs((pred - true) / true)) * 100
-
-
-tsa_render(load_csv())
-
-
-# # %%
 # df = pd.read_csv(sys.argv[1], index_col=0,
 #                      parse_dates=True, usecols=[0, 1])
+
+tsa_render(df)
 
 # %%
